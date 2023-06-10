3faff5c19feb482ed37faae19e8dfd6b47d9a0d9
imjanny <wogns0808@gmail.com>
2023-06-10 17:35:02 +0900

    :sparkles: FEAT : social login button, link
    social login button, button link update

--- static/js/api.js
+++ static/js/api.js
@@ -2,13 +2,13 @@ window.onload = () => {
     console.log('api.js 연결 확인')
 }
 
-// 전체 적용 js
+// 전체 적용 js 
 const frontend_base_url = "http://127.0.0.1:5500"
 const backend_base_url = "http://127.0.0.1:8000"
 
-const payload = localStorage.getItem("payload");
+const payload = localStorage.getItem("payload")
 const payload_parse = JSON.parse(payload);
-const token = localStorage.getItem("access");
+const token = localStorage.getItem("access")
 
 const noProfileImage = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FmC5jq%2FbtshzYZQFIL%2FkKMAW65wigPLiKvVkfQkTk%2Fimg.png"
 
@@ -19,3 +19,4 @@ function handleLogout() {
     localStorage.removeItem("payload");
     location.reload();
 }
+
