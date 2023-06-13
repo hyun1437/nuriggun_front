7d5e23535812aece9086e78a27587e53a4ecef29
Bookwhale00 <Bookwhale00@gmail.com>
2023-06-13 15:16:16 +0900

    :sparkles: Feat: 비밀번호 재설정, 확인 api연결 #5
    password_reset, reset_confirm html,css,js / password_change(변경) 추가, login.css 인풋박스 테두리 보이게 수정

--- static/js/api.js
+++ static/js/api.js
@@ -1,6 +1,3 @@
-window.onload = () => {
-    console.log('api.js 연결 확인')
-}
 
 // 전체 적용 js 
 const frontend_base_url = "http://127.0.0.1:5500"
@@ -12,11 +9,4 @@ const token = localStorage.getItem("access")
 
 const noProfileImage = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FmC5jq%2FbtshzYZQFIL%2FkKMAW65wigPLiKvVkfQkTk%2Fimg.png"
 
-// 로그아웃 
-function handleLogout() {
-    localStorage.removeItem("access");
-    localStorage.removeItem("refresh");
-    localStorage.removeItem("payload");
-    location.reload();
-}
 
