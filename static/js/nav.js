40174373c45a5a8e32110877f8cdd9ca9d60ef5d
hyunah <hil02165@naver.com>
2023-06-21 05:25:06 +0900

    :zap: Feat: footer connect

--- static/js/nav.js
+++ static/js/nav.js
@@ -3,94 +3,106 @@
 // 상단 네비바, 푸터 가져오기
 document.addEventListener("DOMContentLoaded", function () {
 
-    // 네비바를 삽입할 위치
+    // 네비바, 푸터를 삽입할 위치
     const navbar = document.querySelector("#navbar");
-  
+    const footer = document.querySelector("#footer");
+
     if (navbar) {
         // base-nav.html 파일을 가져와서 네비게이션바 위치에 삽입
-    fetch("/base/nav.html")
-        .then(response => response.text())
-        .then(data => {
-            navbar.innerHTML = data;
-        })
-        .then(() => {
+        fetch("/base/nav.html")
+            .then(response => response.text())
+            .then(data => {
+                navbar.innerHTML = data;
+            })
+            .then(() => {
 
-            // nav.html이 로드된 후에 profile_intro 태그와 기타 작업을 수행
-            const payload = localStorage.getItem("payload")
-            const payload_parse = JSON.parse(payload)
-            console.log(payload_parse)
-            const profile_intro = document.getElementById("profile_intro");
-            
-            if (profile_intro) {
-                const payload = localStorage.getItem("payload");
-                const payload_parse = JSON.parse(payload);
-                const profileImage = payload_parse.profile_img ? `${backend_base_url}${payload_parse.profile_img}` : `${noProfileImage}`;
-            
-                profile_intro.innerHTML = `
+                // nav.html이 로드된 후에 profile_intro 태그와 기타 작업을 수행
+                const payload = localStorage.getItem("payload")
+                const payload_parse = JSON.parse(payload)
+                console.log(payload_parse)
+                const profile_intro = document.getElementById("profile_intro");
+
+                if (profile_intro) {
+                    const payload = localStorage.getItem("payload");
+                    const payload_parse = JSON.parse(payload);
+                    const profileImage = payload_parse.profile_img ? `${backend_base_url}${payload_parse.profile_img}` : `${noProfileImage}`;
+
+                    profile_intro.innerHTML = `
                     <a href="${frontend_base_url}/user/profile_page.html?user_id=${payload_parse.user_id}">
                     <span><img class="user-profile-image" src="${profileImage}" alt="" style="width:30px; height:20px;"></span>
                     ${payload_parse.nickname}님
                     </a>`
 
-                let navbarRight = document.getElementById("navbar-right");
-                let newLi = document.createElement("li");
-                newLi.setAttribute("class", "nav-item");
+                    let navbarRight = document.getElementById("navbar-right");
+                    let newLi = document.createElement("li");
+                    newLi.setAttribute("class", "nav-item");
 
-                let logoutBtn = document.createElement("a");
-                logoutBtn.setAttribute("class", "nav-link btn");
-                logoutBtn.innerText = "로그아웃";
-                logoutBtn.setAttribute("href", "#");
+                    let logoutBtn = document.createElement("a");
+                    logoutBtn.setAttribute("class", "nav-link btn");
+                    logoutBtn.innerText = "로그아웃";
+                    logoutBtn.setAttribute("href", "#");
 
-                logoutBtn.setAttribute("onclick", "confirmLogout()");
+                    logoutBtn.setAttribute("onclick", "confirmLogout()");
 
 
 
-                newLi.appendChild(logoutBtn);
+                    newLi.appendChild(logoutBtn);
 
-                navbarRight.appendChild(newLi);
-            }
+                    navbarRight.appendChild(newLi);
+                }
 
-            let loginbtn = document.getElementById("login-btn");
-            let signupbtn = document.getElementById("signup-btn");
-            let createbtn = document.getElementById('create-article')
-            let createnotebtn = document.getElementById('create-note')
-            if (loginbtn) {
-                loginbtn.style.display = "none";
-                signupbtn.style.display = "none";
-                createbtn.style.display = "block";
-                createnotebtn.style.display = "block";
-            }
+                let loginbtn = document.getElementById("login-btn");
+                let signupbtn = document.getElementById("signup-btn");
+                let createbtn = document.getElementById('create-article')
+                let createnotebtn = document.getElementById('create-note')
+                if (loginbtn) {
+                    loginbtn.style.display = "none";
+                    signupbtn.style.display = "none";
+                    createbtn.style.display = "block";
+                    createnotebtn.style.display = "block";
+                }
 
-        })
-        .catch(error => {
-            console.error("Error fetching navigation bar:", error);
-        });
-}
+            })
+            .catch(error => {
+                console.error("Error fetching navigation bar:", error);
+            });
+    }
+
+    if (footer) {
+        // base-footer.html 파일을 가져와서 푸터 위치에 삽입
+        fetch("/base/footer.html")
+            .then(response => response.text())
+            .then(data => {
+                footer.innerHTML = data;
+            })
+            .catch(error => {
+                console.error("Error fetching footer:", error);
+            });
+    }
 });
-  
-  //로그아웃
-  function confirmLogout() {
+
+
+//로그아웃
+function confirmLogout() {
     if (confirm("로그아웃하시겠습니까?")) {
         handleLogout();
     }
-  }
-  async function handleLogout() {
+}
+async function handleLogout() {
     localStorage.removeItem("access")
     localStorage.removeItem("refresh")
     localStorage.removeItem("payload")
     location.replace('/base/index.html')
-    
+}
 
-  }
-  
 
-  
 // 글 작성열기
 function OpenArticle() {
     window.location.href = "/article/create_article.html";
 }
 
+
 // 쪽지 보내기
 function OpenNote() {
-    window.location.href = "/user/note.html";
+    window.location.href = "/user/message.html";
 }
