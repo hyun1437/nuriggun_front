ad6e1ad974eae936f0d62ff9106a10541d04c1a6
Leejunghyun <ark375@naver.com>
2023-07-06 00:03:37 +0900

    :zap: 광고 및 신고 확인 취소 추가
    광고 화면 스크롤 하면 움직입니다. 시도했는데 잘 안되네요 ㅠ 일단 테스트코드가 바빠서 누군가에게 부탁합니다.!!

--- static/js/report.js
+++ static/js/report.js
@@ -2,47 +2,52 @@
 // "신고" 
 
 async function reportUser() {
-    const reportButton = document.getElementById("article-user-url");
-    const user_id = parseInt(reportButton.className);
+  const reportButton = document.getElementById("article-user-url");
+  const user_id = parseInt(reportButton.className);
 
-    // Check if user is logged in
+  // Check if user is logged in
   if (!localStorage.getItem('access')) {
-  alert('로그인 상태에서만 신고가 가능합니다.');
-  return;
+    alert('로그인 상태에서만 신고가 가능합니다.');
+    return;
   }
-  
-    const response = await fetch(`${backend_base_url}/user/report/${user_id}/report/`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('access'),
-      },
-      method: 'POST',
-    });
 
+  // Confirm the report action
+  const confirmReport = confirm('신고 할만큼 화가 났습니까? 그러면 확인을 눌러주세요! 처단하겠습니다!');
+  if (!confirmReport) {
+    return;
+  }
 
-    if (response.ok) {
-      if (response.status === 200) {
-        const responseData = await response.json();
-        if (responseData === '정지된 악질 유저입니다.') {
-          alert(responseData);
-        } else {
-          alert('신고가 접수되었습니다.');
-        }
-      } else if (response.status === 400) {
-        const responseData = await response.json();
-        alert(responseData.message || '이미 신고한 유저입니다.');
-      } else if (response.status === 403) {
-        alert('자신을 신고할 수 없습니다.');
-      }
-    } else {
-      const errorMessage = await response.text();
-      if (response.status === 400) {
-        alert('이미 신고한 유저입니다.');
-      } else if (response.status === 405) {
-        alert('신고 요청을 처리할 수 없습니다.');
+  const response = await fetch(`${backend_base_url}/user/report/${user_id}/report/`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('access'),
+    },
+    method: 'POST',
+  });
+
+  if (response.ok) {
+    if (response.status === 200) {
+      const responseData = await response.json();
+      if (responseData === '정지된 악질 유저입니다.') {
+        alert(responseData);
       } else {
-        alert(`${errorMessage}`);
+        alert('신고가 접수되었습니다.');
       }
+    } else if (response.status === 400) {
+      const responseData = await response.json();
+      alert(responseData.message || '이미 신고한 유저입니다.');
+    } else if (response.status === 403) {
+      alert('자신을 신고할 수 없습니다.');
+    }
+  } else {
+    const errorMessage = await response.text();
+    if (response.status === 400) {
+      alert('이미 신고한 유저입니다.');
+    } else if (response.status === 405) {
+      alert('신고 요청을 처리할 수 없습니다.');
+    } else {
+      alert(`${errorMessage}`);
     }
   }
-  
+}
+
